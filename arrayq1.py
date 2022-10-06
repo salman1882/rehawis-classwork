4381ba953e742172ac615f26a2e6d5085dc56246
salman1882 <113179919+salman1882@users.noreply.github.com>
2022-10-06 12:12:59 +0100

    Create arrayq1.py

---
+++ arrayq1.py
@@ -0,0 +1,15 @@
+numbers = []
+for i in range(7):
+ numbers.append(int(input("Enter num pls ")))
+#next i 
+total = 0
+for index in range(6,-1,-1):
+    print(numbers[index])
+
+print("Total", total)
+primt("Average", total//6)
+
+
+
+
+ 
