a9f1dbdfd2c4fff8936733b1b7ab47ce3282c3e2
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2022-11-06 14:30:55 +0000

    Feedback
    SRC - There should be three folders: Arrays, Queues and lists containing Python code answers to the worksheets. Lot's of work missing here, can you please catch up asap.

--- arrayq1.py
+++ arrayq1.py
@@ -5,6 +5,7 @@ for i in range(7):
 total = 0
 for index in range(6,-1,-1):
     print(numbers[index])
+#next index - make sure you use the pseudocode comments
 
 print("Total", total)
 print("Average", total//6)
