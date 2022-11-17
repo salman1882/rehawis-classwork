4e2b7a331916a87bcd94e95f0db7dcbec06e9416
salman1882 <113179919+salman1882@users.noreply.github.com>
2022-11-17 20:20:54 +0000

    Update FruitsLinkedList.py

--- Lists/FruitsLinkedList.py
+++ Lists/FruitsLinkedList.py
@@ -10,15 +10,20 @@ class LinkedList:
    def listprint(self):
       printval = self.headval
       while printval is not None:
+         global count
+         count = count + 1
          print (printval.name)
-         printval = printval.pointer
-
+         printval = printval.pointer    
+         if printval is None:
+            print(count)
+                
 list = LinkedList()
 list.headval = NodeType("Banana")
 D2 = NodeType("Lemon")
 D3 = NodeType("Melon")
 D4 = NodeType("Strawberry")
 
+count = 0 
 list.headval.pointer = D2
 D2.pointer = D3
 D3.pointer = D4
