9ef9812fd6be128197025e26760e874c9ad8eb62
salman1882 <113179919+salman1882@users.noreply.github.com>
2023-01-13 12:48:19 +0000

    Add files via upload

---
+++ Searches and sorts/bubblesort.py
@@ -0,0 +1,16 @@
+def bubbleSort(arr):
+    Length = len(arr)
+    for i in range(Length-1):
+        for j in range(0, Length-i-1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+         
+    return arr
+ 
+ 
+
+arr = [4, 24, 45, 72, 13, 23]
+ 
+print(bubbleSort(arr))
+ 
